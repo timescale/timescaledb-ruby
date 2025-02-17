16a00796106d1ef64c51c130b05e296ea658cbeb
Jônatas Davi Paganini <jonatasdp@gmail.com>
2025-02-17 19:21:00 -0300

    Setup connection

--- lib/timescaledb/connection_handling.rb
+++ lib/timescaledb/connection_handling.rb
@@ -5,12 +5,23 @@ module Timescaledb
   
   # @param [String] config with the postgres connection string.
   def establish_connection(config)
+    # Establish connection for Timescaledb
     Connection.instance.config = config
+    
+    # Also establish connection for ActiveRecord if it's defined
+    if defined?(ActiveRecord::Base)
+      ActiveRecord::Base.establish_connection(config)
+    end
   end
 
   # @param [PG::Connection] to use it directly from a raw connection
   def use_connection conn
     Connection.instance.use_connection conn
+    
+    # Also set ActiveRecord connection if it's defined
+    if defined?(ActiveRecord::Base)
+      ActiveRecord::Base.connection.raw_connection = conn
+    end
   end
 
   def connection
